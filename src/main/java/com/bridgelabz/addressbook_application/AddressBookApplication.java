a28eee32e65e00a5146080fb58168fe05e640a23
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2022-02-13 23:18:42 +0530

    Created Address Book App Spring Project

---
+++ src/main/java/com/bridgelabz/addressbook_application/AddressBookApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.addressbook_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+    /*UC-1*/
+    /* Creating an Address Book Spring Project to  REST Request from Address Book */
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
+
+}
